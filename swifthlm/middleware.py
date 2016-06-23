5b6ca11431b67e3b7296ea0e0fc32f4f07283461
Harald Seipp <seipp@de.ibm.com>
2016-06-23 17:43:49 +0200

    Cleanup imports, ensure PEP8 compliance

--- swifthlm/middleware.py
+++ swifthlm/middleware.py
@@ -90,55 +90,68 @@ Slavisa Sarafijanovic (sla@zurich.ibm.com)
 Harald Seipp (seipp@de.ibm.com)
 """
 
-import errno
 import subprocess
 import random
 import string
-import os
-import sys
 
 from swift.common.swob import Request, Response
 from swift.common.http import HTTP_OK
 from swift.common.utils import register_swift_info
 
-# If POST is migration or recall request, or GET is status request, process by this middleware 
 class HlmMiddleware(object):
 
     def __init__(self, app, conf):
         # app is the final application
         self.app = app
         # Read settings from proxy-server.conf
-        self.migrate_backend = conf.get('migrate_backend','/opt/ibm/swift-hlm-backend/migrate')
-        self.recall_backend = conf.get('recall_backend','/opt/ibm/swift-hlm-backend/recall')
-        self.status_backend = conf.get('status_backend','/opt/ibm/swift-hlm-backend/status')
+        self.migrate_backend = conf.get('migrate_backend',
+                                        '/opt/ibm/swift-hlm-backend/migrate')
+        self.recall_backend = conf.get('recall_backend',
+                                       '/opt/ibm/swift-hlm-backend/recall')
+        self.status_backend = conf.get('status_backend',
+                                       '/opt/ibm/swift-hlm-backend/status')
 
     def __call__(self, env, start_response):
         # Process POST request to migrate/recall object(s)
         if env['REQUEST_METHOD'] == "POST":
             path = env['PATH_INFO']
             query = env.get('QUERY_STRING') or ''
-            requestId = ''.join(random.choice(string.digits) for i in range(12))
+            requestId = ''.join(random.choice(string.digits)
+                                for i in range(12))
             if query == 'MIGRATE':
                 subprocess.call([self.migrate_backend, path[4:], requestId])
-                return Response(status=HTTP_OK, body="Accepted migration request.\n", content_type="text/plain")(env, start_response)
+                return Response(status=HTTP_OK,
+                                body="Accepted migration request.\n",
+                                content_type="text/plain")(env, start_response)
 
             if query == 'RECALL':
                 subprocess.call([self.recall_backend, path[4:], requestId])
-                return Response(status=HTTP_OK, body="Accepted recall request.\n", content_type="text/plain")(env, start_response)
+                return Response(status=HTTP_OK,
+                                body="Accepted recall request.\n",
+                                content_type="text/plain")(env, start_response)
+
         # Process GET request to get object(s) status
         if env['REQUEST_METHOD'] == "GET":
             path = env['PATH_INFO']
             query = env.get('QUERY_STRING') or ''
-            requestId = ''.join(random.choice(string.digits) for i in range(12)) # TBD: support requestId as input 
+            # TBD: support requestId as input
+            requestId = ''.join(random.choice(string.digits)
+                                for i in range(12))
             if 'STATUS' in query:
                 try:
-		    out = subprocess.check_output([self.status_backend, path[4:], requestId]) #py 2.7+
+                    out = subprocess.check_output([self.status_backend,
+                                                   path[4:],
+                                                   requestId,
+                                                   query])  # py 2.7+
                 except subprocess.CalledProcessError, e:
-		    out = e.output
-                return Response(status=HTTP_OK, body=out, content_type="text/plain")(env, start_response)
+                    out = e.output
+                return Response(status=HTTP_OK,
+                                body=out,
+                                content_type="text/plain")(env, start_response)
 
         return self.app(env, start_response)
-              	
+
+
 def filter_factory(global_conf, **local_conf):
     conf = global_conf.copy()
     conf.update(local_conf)
